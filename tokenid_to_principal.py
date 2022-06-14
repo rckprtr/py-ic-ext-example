37d5cc713ad7f412445713e105d74192a276e5a2
Rick Porter <rick.e.porter@gmail.com>
2022-06-14 14:07:41 -0700

    tokenid to principal

--- tokenid_to_principal.py
+++ tokenid_to_principal.py
@@ -8,3 +8,18 @@
 #   console.log(array);
 #   return Principal.fromUint8Array(array).toText();
 # };
+
+
+# tokenIdentifier(canister_id, index)
+# tokenIdentifier("bzsui-sqaaa-aaaah-qce2a-cai", 4350) -> aaaa-a
+#
+#  byteArray = ["\x0Atid" + canister_id.bytes + Index.bytes]
+#  return Principal.fromBytes(byteArray)
+#
+
+
+# index = 4350
+# reg_p = Principal.from_str("bzsui-sqaaa-aaaah-qce2a-cai").bytes
+# output = b"\x0Atid"+reg_p + index.to_bytes(4, 'big')
+# result = Principal.from_hex(bytes.hex(output))
+# print(result)
